80e1e2d126e039d9075cd667665c41c3d0859d46
Andrey Mashukov <andre.mashukov@gmail.com>
2020-02-03 17:21:59 +0800

    Change directory structure

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { ExpressServer } from './server';
 
 /**
- * Wrapper around the Node process, ExpressServer abstraction and complex dependencies such as services that ExpressServer needs.
- * When not using Dependency Injection, can be used as place for wiring together services which are dependencies of ExpressServer.
+ * Wrapper around the Node process, ExpressServer abstraction
+ * and complex dependencies such as services that ExpressServer needs.
+ * When not using Dependency Injection, can be used as place
+ * for wiring together services which are dependencies of ExpressServer.
  */
 export class Application {
     public static async createApplication() {
@@ -23,11 +25,11 @@ export class Application {
             Application.shutdownProperly(2, express);
         });
         process.on('SIGINT', () => {
-            console.info('Caught SIGINT')
+            console.info('Caught SIGINT');
             Application.shutdownProperly(128 + 2, express);
         });
         process.on('SIGTERM', () => {
-            console.info('Caught SIGTERM')
+            console.info('Caught SIGTERM');
             Application.shutdownProperly(128 + 2, express);
         });
         process.on('exit', () => {
