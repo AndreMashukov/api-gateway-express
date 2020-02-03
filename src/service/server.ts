fc1cc79b0f1e3ddb58580fd460b228db39a5bf25
Andrey Mashukov <andre.mashukov@gmail.com>
2020-02-03 14:08:58 +0800

    First commit

---
+++ src/service/server.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import { Express } from 'express';
+import { Server } from 'http';
+
+/**
+ * Abstraction around the raw Express.js server and Nodes' HTTP server.
+ * Defines HTTP request mappings, basic as well as request-mapping-specific
+ * middleware chains for application logic, config and everything else.
+ */
+export class ExpressServer {
+    private server?: Express;
+    private httpServer?: Server;
+
+    public async setup(port: number) {
+        const server = express();
+        this.httpServer = this.listen(server, port);
+        this.server = server;
+        return this.server;
+    }
+
+    public listen(server: Express, port: number) {
+        return server.listen(port);
+    }
+
+    public kill() {
+        if (this.httpServer) this.httpServer.close();
+    }
+}
